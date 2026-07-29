a77f9483d45bdbe6f2a68f244169d60b0c3e79b3
huperr <thehien19121003@gmail.com>
2026-07-29 12:36:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const display = document.getElementById("input"); //the result
 //function time >:D
 
 function press(input) {
-  input.value += input //this thing put a number or a character into the display text
+  display.value += input //this thing put a number or a character into the display text
 }
 
 function clear() {
