3be7ce60dfa30461f1ea38d402d9e6d439ce20a4
huperr <thehien19121003@gmail.com>
2026-07-29 12:32:17 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+//time to code!
+const display = document.getElementById("input"); //the result
+
+//function time >:D
+
+function press(input) {
+  input.value += input //this thing put a number or a character into the display text
+}
+
+function clear() {
+  
+}
+
+function calculate() {
+  
+}
